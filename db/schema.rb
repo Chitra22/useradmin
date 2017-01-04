894b4a711d3d23089b81c2320c268939b1fbe393
Senthil <senthilkumarn@sowdambikaengg.edu.in>
2017-01-04 13:52:17 +0530

    senthi-brnach

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20170104064418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+
   create_table "comments", force: :cascade do |t|
     t.string   "name"
     t.text     "body"
