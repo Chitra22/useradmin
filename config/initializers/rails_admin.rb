81fcce9a22cdef5d80128bdf729877d512b30c9a
senthilalagar <senthilkumarn@sowdambikaengg.edu.in>
2017-02-03 17:14:02 +0530

    solved rails_Admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,5 @@
 RailsAdmin.config do |config|
-
+config.included_models = [User, Post, Comment]
    config.authorize_with do
     redirect_to main_app.root_path unless warden.user.admin == true
   end
